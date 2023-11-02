fd6e08020c5160a12386a059863ae67fea26e556
Santhoshgithub22 <santhoshbharath61@gmail.com>
2023-11-02 11:03:12 +0530

    Model Training In Notebooks

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ HYPEN_E_DOT = "-e ."
 def get_requirements(filepath:str)->List[str]:
 
     requirements = []
-
+    
     with open(filepath) as file_obj:
         requirements = file_obj.readlines()
         requirements = [req.replace("/n", "") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
-            
+
     return requirements
 
 setup(
